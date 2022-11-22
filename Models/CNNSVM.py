970f7e0a1f65dd5e33b4e38a42f3e47c25329e55
ruuush07 <30756824+RushilPatel0703@users.noreply.github.com>
2022-11-22 21:49:06 +0200

    added more comments

--- Models/CNNSVM.py
+++ Models/CNNSVM.py
@@ -6,28 +6,33 @@ class CNNSVM(tf.keras.Model):
   def __init__(self):
     super(CNNSVM, self).__init__()
 
-    self.norm = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
-
+    self.norm = tf.keras.layers.experimental.preprocessing.Rescaling(1./255) #normalisation of data
+  
+    #first convolution layer
     self.conv1 = tf.keras.layers.Conv2D(32, 3, activation='relu')
     self.max1 = tf.keras.layers.MaxPooling2D()
     self.drop1 = tf.keras.layers.Dropout(0.2)
-
+    
+    #second convolution layer
     self.conv2 = tf.keras.layers.Conv2D(32, 3, activation='relu')
     self.max2 = tf.keras.layers.MaxPooling2D()
     self.drop2 = tf.keras.layers.Dropout(0.2)
 
+    #third convolution layer
     self.conv3 = tf.keras.layers.Conv2D(32, 3, activation='relu')
     self.max3 = tf.keras.layers.MaxPooling2D()
     self.drop3 = tf.keras.layers.Dropout(0.2)
 
     self.flat = tf.keras.layers.Flatten()
-
+    
+    #fully connected layer
     self.dense1 = tf.keras.layers.Dense(256, activation='relu')
     self.dense2 = tf.keras.layers.Dense(256, activation='relu')
     self.drop4 = tf.keras.layers.Dropout(0.2)
     self.dense3 = tf.keras.layers.Dense(128, activation='relu')
-
-    self.svm = tf.keras.layers.experimental.RandomFourierFeatures(output_dim=7, kernel_initializer='gaussian', trainable=True)
+    
+    # SVM layer
+    self.svm = tf.keras.layers.experimental.RandomFourierFeatures(output_dim=7, kernel_initializer='gaussian', trainable=True)#using Gaussian Radial Basis Function
 
   def call(self, x):
     x = self.norm(x)
