721514b8dc9536d4a7b7a74ce72114b6c88402c6
ruuush07 <30756824+RushilPatel0703@users.noreply.github.com>
2022-11-22 21:47:00 +0200

    added comments

--- Models/CNN.py
+++ Models/CNN.py
@@ -5,20 +5,25 @@ class CNN(tf.keras.Model):
   def __init__(self):
     super(CNN, self).__init__()
 
-    self.norm = tf.keras.layers.experimental.preprocessing.Rescaling(1./255)
-
+    self.norm = tf.keras.layers.experimental.preprocessing.Rescaling(1./255) # normalising the input
+    
+    #first convolution layer
     self.conv1 = tf.keras.layers.Conv2D(32, 3, activation='relu')
     self.max1 = tf.keras.layers.MaxPooling2D()
-
+    
+    #second convolution layer
     self.conv2 = tf.keras.layers.Conv2D(32, 3, activation='relu')
     self.max2 = tf.keras.layers.MaxPooling2D()
-
+    
+    #third convolution layer
     self.conv3 = tf.keras.layers.Conv2D(32, 3, activation='relu')
     self.max3 = tf.keras.layers.MaxPooling2D()
 
+    #Fully connected layer
     self.flat = tf.keras.layers.Flatten()
     self.dense1 = tf.keras.layers.Dense(128, activation='softmax')
     self.dropout = tf.keras.layers.Dropout(0.3)
+    
     self.dense2 = tf.keras.layers.Dense(7)
 
   def call(self, x):
